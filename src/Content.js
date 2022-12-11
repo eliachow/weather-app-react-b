d9e39a3660cec13f2cafe420d53bc221eaf8117e
eliachow <eliacchow@gmail.com>
2022-12-11 14:39:34 -0800

    Updated Styling

--- src/Content.js
+++ src/Content.js
@@ -1,14 +1,12 @@
 import React, { useState } from "react";
 import "./Content.css";
 import axios from "axios";
-import FormattedDate from "./FormattedDate";
+import WeatherInfo from "./WeatherInfo";
 import cloudVideo from "./media/cloudVideo.mp4";
-import { WiHumidity } from "weather-icons-react";
-import { WiUmbrella } from "weather-icons-react";
-import { WiCloudyGusts } from "weather-icons-react";
 
-export default function Content() {
+export default function Content(props) {
   const [weatherData, setWeatherData] = useState({ ready: false });
+  const [city, setCity] = useState(props.defaultCity);
 
   function handleResponse(response) {
     console.log(response.data);
@@ -24,6 +22,15 @@ export default function Content() {
     });
   }
 
+  function handleSubmit(event) {
+    event.preventDefautl();
+    //search for a city
+  }
+
+  function handleCityChange(event) {
+    setCity(event.target.value);
+  }
+
   if (weatherData.ready) {
     return (
       <div className="Content">
@@ -31,13 +38,14 @@ export default function Content() {
         <div className="PageContent">
           <div className="Header">
             <div className="HeaderTitle">Weather Forecast App</div>
-            <form className="row SearchBar">
+            <form className="row SearchBar" onSubmit={handleSubmit}>
               <div className="col-6">
                 <input
-                  type="text"
+                  type="search"
                   className="form-control button"
                   placeholder="Enter a city"
                   autoFocus="on"
+                  onChange={handleCityChange}
                 />
               </div>
               <div className="col-3">
@@ -56,171 +64,7 @@ export default function Content() {
               </div>
             </form>
 
-            <div className="CenterContent">
-              <div className="row">
-                <div className="col-7">
-                  <div className="MainData">
-                    <h1>{weatherData.city}</h1>
-                    <h3>
-                      <span>
-                        <FormattedDate date={weatherData.date} />
-                      </span>
-                    </h3>
-                    <div className="row">
-                      <div className="col-5">
-                        <img
-                          src={weatherData.iconUrl}
-                          alt={weatherData.description}
-                        ></img>
-                      </div>
-                      <div className="col-6">
-                        <span className="MainTemp">
-                          {Math.round(weatherData.temperature)}{" "}
-                        </span>
-                        <span>ᵒC</span> | <span>ᵒF</span>
-                        <div>
-                          <span>
-                            <WiUmbrella size={40} color="white" />
-                          </span>
-                          <span className="text-capitalize">
-                            {weatherData.description}
-                          </span>
-                        </div>
-                        <div>
-                          <span>
-                            <WiHumidity size={40} color="white" />
-                          </span>
-                          Humidity: <span>{weatherData.humidity}%</span>
-                        </div>
-                        <div>
-                          <span>
-                            <WiCloudyGusts size={40} color="white" />
-                          </span>
-                          Wind Speed:{" "}
-                          <span>{Math.round(weatherData.wind)} kmh</span>
-                        </div>
-                      </div>
-                    </div>
-                    <br />
-                    <br />
-                    <div className="Hourly">
-                      <h3>Hourly Forecast</h3>
-                      <div className="row">
-                        <div className="col-sm HourlyData">
-                          <img
-                            src={weatherData.iconUrl}
-                            alt={weatherData.description}
-                          ></img>
-                          <div>Hour</div>
-                          <div>5 °C</div>
-                        </div>
-                        <div className="col-sm HourlyData">
-                          <img
-                            src={weatherData.iconUrl}
-                            alt={weatherData.description}
-                          ></img>
-                          <div>Hour</div>
-                          <div>5 °C</div>
-                        </div>
-                        <div className="col-sm HourlyData">
-                          <img
-                            src={weatherData.iconUrl}
-                            alt={weatherData.description}
-                          ></img>
-                          <div>Hour</div>
-                          <div>5 °C</div>
-                        </div>
-                        <div className="col-sm HourlyData">
-                          <img
-                            src={weatherData.iconUrl}
-                            alt={weatherData.description}
-                          ></img>
-                          <div>Hour</div>
-                          <div>5 °C</div>
-                        </div>
-                        <div className="col-sm HourlyData">
-                          <img
-                            src={weatherData.iconUrl}
-                            alt={weatherData.description}
-                          ></img>
-                          <div>Hour</div>
-                          <div>5 °C</div>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-                <div className="col-5">
-                  <div className="col-6">
-                    <h3>5 Day Forecast</h3>
-                    <div>
-                      <ul className="EachDay">
-                        <li className="card">
-                          <div className="row">
-                            <div className="col-4 day">Sunday</div>
-                            <div className="col-4">
-                              <img
-                                src={weatherData.iconUrl}
-                                alt={weatherData.description}
-                              ></img>
-                            </div>
-                            <div className="col-4">5ᵒ</div>
-                          </div>
-                        </li>
-                      </ul>
-                      <ul className="EachDay">
-                        <li className="card">
-                          <span>
-                            Sunday
-                            <img
-                              src={weatherData.iconUrl}
-                              alt={weatherData.description}
-                            ></img>
-                            5ᵒ
-                          </span>
-                        </li>
-                      </ul>
-                      <ul className="EachDay">
-                        <li className="card">
-                          <span>
-                            Sunday
-                            <img
-                              src={weatherData.iconUrl}
-                              alt={weatherData.description}
-                            ></img>
-                            5ᵒ
-                          </span>
-                        </li>
-                      </ul>
-                      <ul className="EachDay">
-                        <li className="card">
-                          <span>
-                            Sunday
-                            <img
-                              src={weatherData.iconUrl}
-                              alt={weatherData.description}
-                            ></img>
-                            5ᵒ
-                          </span>
-                        </li>
-                      </ul>
-                      <ul className="EachDay">
-                        <li className="card">
-                          <span>
-                            Sunday
-                            <img
-                              src={weatherData.iconUrl}
-                              alt={weatherData.description}
-                            ></img>
-                            5ᵒ
-                          </span>
-                        </li>
-                      </ul>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
+            <WeatherInfo data={weatherData} />
           </div>
         </div>
       </div>
@@ -229,9 +73,8 @@ export default function Content() {
     const apiKey = "291d093572471cc9cd6958074405d546";
     let city = "Vancouver";
     let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
-    console.log(apiUrl);
     axios.get(apiUrl).then(handleResponse);
-
+    console.log(apiUrl);
     return "Loading...";
   }
 }
---
+++ src/WeatherInfo.js
@@ -0,0 +1,187 @@
+import React from "react";
+import "./WeatherInfo.css";
+import FormattedDate from "./FormattedDate";
+
+import { WiHumidity } from "weather-icons-react";
+import { WiUmbrella } from "weather-icons-react";
+import { WiCloudyGusts } from "weather-icons-react";
+
+export default function WeatherInfo(props) {
+  return (
+    <div className="WeatherInfo">
+      <div className="CenterContent">
+        <div className="row">
+          <div className="col-7">
+            <div className="MainData">
+              <h1 className="MainCity">{props.data.city}</h1>
+              <h3>
+                <span>
+                  <FormattedDate date={props.data.date} />
+                </span>
+              </h3>
+              <div className="row MainIconAndData">
+                <div className="col-5">
+                  <img
+                    src={props.data.iconUrl}
+                    alt={props.data.description}
+                    className="MainWeatherIcon"
+                  ></img>
+                </div>
+                <div className="col-6">
+                  <span className="MainTemp">
+                    {Math.round(props.data.temperature)}{" "}
+                  </span>
+                  <span>ᵒC</span> | <span>ᵒF</span>
+                  <div>
+                    <span>
+                      <WiUmbrella size={40} color="white" />
+                    </span>
+                    <span className="text-capitalize">
+                      {props.data.description}
+                    </span>
+                  </div>
+                  <div>
+                    <span>
+                      <WiHumidity size={40} color="white" />
+                    </span>
+                    Humidity: <span>{props.data.humidity}%</span>
+                  </div>
+                  <div>
+                    <span>
+                      <WiCloudyGusts size={40} color="white" />
+                    </span>
+                    Wind Speed: <span>{Math.round(props.data.wind)} kmh</span>
+                  </div>
+                </div>
+              </div>
+              <br />
+              <br />
+              <div className="Hourly">
+                <h3 className="HourlyTitle">Hourly Forecast</h3>
+                <div className="row">
+                  <div className="col-sm HourlyData">
+                    <img
+                      src={props.data.iconUrl}
+                      alt={props.data.description}
+                    ></img>
+                    <div>Hour</div>
+                    <div>5 °C</div>
+                  </div>
+                  <div className="col-sm HourlyData">
+                    <img
+                      src={props.data.iconUrl}
+                      alt={props.data.description}
+                    ></img>
+                    <div>Hour</div>
+                    <div>5 °C</div>
+                  </div>
+                  <div className="col-sm HourlyData">
+                    <img
+                      src={props.data.iconUrl}
+                      alt={props.data.description}
+                    ></img>
+                    <div>Hour</div>
+                    <div>5 °C</div>
+                  </div>
+                  <div className="col-sm HourlyData">
+                    <img
+                      src={props.data.iconUrl}
+                      alt={props.data.description}
+                    ></img>
+                    <div>Hour</div>
+                    <div>5 °C</div>
+                  </div>
+                  <div className="col-sm HourlyData">
+                    <img
+                      src={props.data.iconUrl}
+                      alt={props.data.description}
+                    ></img>
+                    <div>Hour</div>
+                    <div>5 °C</div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="col-5">
+            <div className="col-6">
+              <h3 className="FiveDayTitle">5 Day Forecast</h3>
+              <div>
+                <ul className="EachDay">
+                  <li className="card">
+                    <div className="row">
+                      <div className="col-4 Day">Sunday</div>
+                      <div className="col-4">
+                        <img
+                          src={props.data.iconUrl}
+                          alt={props.data.description}
+                        ></img>
+                      </div>
+                      <div className="col-4 DayTemp">5ᵒ</div>
+                    </div>
+                  </li>
+                </ul>
+                <ul className="EachDay">
+                  <li className="card">
+                    <div className="row">
+                      <div className="col-4 Day">Sunday</div>
+                      <div className="col-4">
+                        <img
+                          src={props.data.iconUrl}
+                          alt={props.data.description}
+                        ></img>
+                      </div>
+                      <div className="col-4 DayTemp">5ᵒ</div>
+                    </div>
+                  </li>
+                </ul>
+                <ul className="EachDay">
+                  <li className="card">
+                    <div className="row">
+                      <div className="col-4 Day">Sunday</div>
+                      <div className="col-4">
+                        <img
+                          src={props.data.iconUrl}
+                          alt={props.data.description}
+                        ></img>
+                      </div>
+                      <div className="col-4 DayTemp">5ᵒ</div>
+                    </div>
+                  </li>
+                </ul>
+                <ul className="EachDay">
+                  <li className="card">
+                    <div className="row">
+                      <div className="col-4 Day">Sunday</div>
+                      <div className="col-4">
+                        <img
+                          src={props.data.iconUrl}
+                          alt={props.data.description}
+                        ></img>
+                      </div>
+                      <div className="col-4 DayTemp">5ᵒ</div>
+                    </div>
+                  </li>
+                </ul>
+                <ul className="EachDay">
+                  <li className="card">
+                    <div className="row">
+                      <div className="col-4 Day">Sunday</div>
+                      <div className="col-4">
+                        <img
+                          src={props.data.iconUrl}
+                          alt={props.data.description}
+                        ></img>
+                      </div>
+                      <div className="col-4 DayTemp">5ᵒ</div>
+                    </div>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
